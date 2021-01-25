1cfd9df221d61393529b35fa98d89c78c38bfa05
outstandingboy <outstanding1301@gmail.com>
2021-01-25 20:28:37 +0900

    feat: 라우터 설저

--- src/App.js
+++ src/App.js
@@ -1,8 +1,39 @@
-import Chatroom from './components/Chatroom';
+import { Grid, makeStyles } from '@material-ui/core';
+import ChatroomPage from 'page/ChatroomPage';
+import IndexPage from 'page/IndexPage';
+import { Route, HashRouter as Router, Switch } from 'react-router-dom';
+
+import Sidebar from 'components/sidebar/Sidebar'
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    height: '100vh'
+  },
+  drawer: {
+    backgroundColor: '#350C35',
+    height: '100%'
+  }
+}));
+
 
 function App() {
+  const classes = useStyles();
   return (
-    <Chatroom room="aba4647" oauth={process.env.REACT_APP_OAUTH_KEY || ""}/>
+    <Router>
+      <Grid container className={classes.root}>
+        <Grid item xs={2} className={classes.drawer}>
+          <Sidebar />
+        </Grid>
+        <Grid item xs={10}>
+          <Switch>
+            <Route path="/@:room" component={ChatroomPage}/>
+            <Route exact path="/">
+                  <IndexPage />
+            </Route>
+          </Switch>
+        </Grid>
+      </Grid>
+    </Router>
   );
 }
 
