e7862e7c2905426b82f907dff8314d4855f9e119
outstandingboy <outstanding1301@gmail.com>
2021-01-28 02:57:29 +0900

    style: 채팅 디자인 수정

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,4 +1,22 @@
+import { Button, withStyles } from '@material-ui/core';
 import React from 'react';
+
+const useStyles = {
+    container: {
+        display: 'flex',
+        margin: '5px'
+    },
+    user: {
+        display: 'inline-block',
+        maxWidth: '300px',
+        whiteSpace: 'nowrap',
+        overflow: 'hidden',
+        textOverflow: 'ellipsis'
+    },
+    chat: {
+        flex: 1,
+    }
+};
 
 class Chat extends React.Component {
     constructor(props) {
@@ -6,12 +24,22 @@ class Chat extends React.Component {
     }
     render() {
         const {chat} = this.props;
+        const {classes} = this.props;
         return (
-            <div>
-                {chat['display-name']}({chat['username']}): {chat['message']}
+            <div className={classes.container}>
+                <div>
+                <Button variant="contained" className={classes.user} style={{backgroundColor: this.props.backgroundColor, color: "#FFFFFF"}}>
+                    {chat['display-name']}({chat['username']})
+                </Button>
+                </div>
+                <div className={classes.chat}>
+                <Button variant="contained" style={{backgroundColor: '#FFFFFF'}}>
+                    {chat['message']}
+                </Button>
+                </div>
             </div>
         );
     }
 }
 
-export default Chat;
+export default withStyles(useStyles)(Chat);
